2f79e595cb49d3df4a57a2350a358243bc01b6ba
Emma <emma@cyborggrizzly.com>
2025-01-19 03:42:06 -0700

    trim

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,7 @@
-import { useEffect, useState } from "preact/hooks";
-
-type Record = {
-  date: string;
-  hour: string;
-  speed: string;
-};
-
 export function App() {
-  const [records, setRecords] = useState<Record[]>([]);
-
-  useEffect(() => {
-    fetch(`/json/get-day?date=${new Date().toISOString().split("T")[0]}`)
-      .then((res) => res.json())
-      .then((data) => setRecords(data));
-  }, []);
-
   return (
     <>
-      <h1>Speedtest</h1>
-      {records.length === 0 && <p class="">Loading...</p>}
-      <ul>
-        {records.map((record) => (
-          <li>{record.date} {record.hour} {record.speed}</li>
-        ))}
-      </ul>
+      <h1 class="intellisense no worky here">Repro</h1>
     </>
   );
 }
